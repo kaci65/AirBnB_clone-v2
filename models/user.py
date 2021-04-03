36bd6c653c9c41f8f5678255b2001841268a2faf
kaci65 <wanjikukarugi@gmail.com>
2021-04-03 22:26:53 +0300

    Fix file: add sqlalchemy import, fix model import, fix tablename

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
@@ -8,7 +9,7 @@ class User(BaseModel, Base):
     This class defines a user by various attributes
     Inherits from BaseModel and Base
     """
-    __tablename = "users"
+    __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
