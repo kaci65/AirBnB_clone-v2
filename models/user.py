fc1770a46165886b1ec7a5a340c408984440dcb3
kaci65 <wanjikukarugi@gmail.com>
2021-04-03 22:22:25 +0300

    Update file: class attributes(tablename, email, password, first_name, last_name

--- models/user.py
+++ models/user.py
@@ -3,9 +3,14 @@
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    This class defines a user by various attributes
+    Inherits from BaseModel and Base
+    """
+    __tablename = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
